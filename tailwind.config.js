bbabd9fbc38c1f22d125f86a6eec167770bed0f1
clarismetaverse <134530419+clarismetaverse@users.noreply.github.com>
2026-03-07 22:58:51 +0100

    Migrate tailwind.config.js to ES module syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
